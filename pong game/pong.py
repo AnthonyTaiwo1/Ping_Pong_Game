716f7042ea417bd865b105a615ead84cf4a22741
AnthonyTaiwo1 <69257231+AnthonyTaiwo1@users.noreply.github.com>
2020-08-16 15:13:56 +0100

    Add files via upload

---
+++ pong game/pong.py
@@ -0,0 +1,132 @@
+import turtle # good for making simple games
+import winsound
+import playsound
+
+# window specification
+win= turtle.Screen()
+win.title("pong by Anthony Taiwo")
+win.bgcolor("blue")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+#Score
+score_a = 0
+score_b = 0
+
+
+#paddle A specs
+
+paddle_a= turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("black")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # changes the shapes size from the default
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+#paddle B specs
+paddle_b= turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("black")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) #changes the shapes size from the default
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+
+#Ball specs
+ball= turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("black")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.2                  # ball moves up by 1 px
+ball.dy= 0.2                   # ball moves right by 1 px
+
+#pen
+pen=turtle.Turtle()
+pen.speed(0)
+pen.color("White")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Functions
+
+def paddle_a_up():
+    y = paddle_a.ycor()                              # this returns the y coordinate
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()                              # this returns the y coordinate
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()                              # this returns the y coordinate
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()                              # this returns the y coordinate
+    y -= 20
+    paddle_b.sety(y)    
+
+# keyboard binding
+
+win.listen()                                        # this listens for a skeyboard input
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+#main game loop
+while True:
+    win.update()     # stops the page from refreshing
+
+# ball movement    
+    ball.setx(ball.xcor() + ball.dx) # ball moves 1 to the right in loop
+    ball.sety(ball.ycor() + ball.dy) # ball moves 1 to the right in loop
+
+
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *=-1
+        winsound.PlaySound("bounce.wav",True)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *=-1 
+        winsound.PlaySound("bounce.wav",True)
+
+    if ball.xcor() > 390:
+        ball.goto(0,0) 
+        ball.dx *=-1  
+        score_a += 1
+        pen.clear()    
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("smashing.wav",True)
+
+    if ball.xcor() < -390:
+        ball.goto(0,0) 
+        ball.dx *=-1  
+        score_b += 1
+        pen.clear() 
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal")) 
+        winsound.PlaySound("smashing.wav",True) 
+
+    # paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *=-1
+        winsound.PlaySound("bounce.wav",True)
+        
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *=-1
+        winsound.PlaySound("bounce.wav",True)          
